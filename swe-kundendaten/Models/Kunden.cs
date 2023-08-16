bf712702c4ac4a75a04a94cb769efbccdf163314
Erich Bosak <bosake032@sus-am-bktm.de>
2023-08-16 09:13:13 +0200

    Unterricht 16.08
    Einführung in Enitity Framework

---
+++ swe-kundendaten/Models/Kunden.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace swe_kundendaten.Models
+{
+    public partial class Kunden
+    {
+        public int Id { get; set; }
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public string Email { get; set; }
+        public int ZustimmungWerbung { get; set; }
+    }
+}
---
+++ swe-kundendaten/Models/kundenverwaltungContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace swe_kundendaten.Models
+{
+    public partial class kundenverwaltungContext : DbContext
+    {
+        public kundenverwaltungContext()
+        {
+        }
+
+        public kundenverwaltungContext(DbContextOptions<kundenverwaltungContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Kunden> Kunden { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=.;Database=kundenverwaltung;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Kunden>(entity =>
+            {
+                entity.ToTable("kunden");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasColumnName("email")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nachname)
+                    .IsRequired()
+                    .HasColumnName("nachname")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Vorname)
+                    .IsRequired()
+                    .HasColumnName("vorname")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ZustimmungWerbung).HasColumnName("zustimmung_werbung");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
